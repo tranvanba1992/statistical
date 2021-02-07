2b20440bb3575db2ebffad849cb48de5201d4808
BaTV <batv@tohsoft.com>
2021-02-07 15:52:14 +0700

    css

--- src/Http/Controllers/StatisticalController.php
+++ src/Http/Controllers/StatisticalController.php
@@ -12,7 +12,7 @@ class StatisticalController extends Controller
      */
     public function getIndex()
     {
-        dd(333333);
+        dd(101010101);
         Statistical::create(['name' => 'Statistical ' . time()]);
         return view('toh-statistical::index');
     }
